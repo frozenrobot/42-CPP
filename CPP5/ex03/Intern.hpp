8d003152a3a0a644d95674d4837f4e6b8322f845
Khanak Gulati <khanakgulati@Khanaks-Air.fritz.box>
2021-09-19 14:07:40 +0200

    cpp5 intern

--- CPP5/ex03/Intern.hpp
+++ CPP5/ex03/Intern.hpp
@@ -8,7 +8,32 @@
 
 class Intern
 {
-    
+    private:
+        // class FormMatch
+        // {
+        //     private:
+        //         std::string name;
+        //         std::string target;
+        //     public:
+        //         FormMatch(std::string name, std::string target);
+        //         Form *execute(std::string type)
+        // };
+        std::string name;
+        std::string target;
+        std::string matches[3];
+    public:
+        Intern();
+        Intern(Intern const &toCopy);
+        Intern &operator=(Intern const &toAssign);
+        virtual ~Intern();
+        Form *makeForm(std::string const &formName, std::string const &target) const;
+
+        class NonExistentFormException: public std::exception {
+            virtual const char* what() const throw()
+            {
+                return "EXCEPTION: Form does not exist, check syntax.";
+            }
+        };
 };
 
 #endif
