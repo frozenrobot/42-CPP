5dbaf2988cebf332b672345de9c82c5ccf725b18
Khanak Gulati <khanakgulati@RN-145-97-136-200.eduroam.rug.nl>
2021-09-20 18:14:05 +0200

    all

--- CPP4/ex03/AMateria.cpp
+++ CPP4/ex03/AMateria.cpp
@@ -52,14 +52,14 @@ void Ice::use(ICharacter &target)
 }
 
 
-Cure::Cure(void) : AMateria("ice")
+Cure::Cure(void) : AMateria("cure")
 {
     return;
 }
 
-Cure::Cure(Cure const & other): AMateria("ice")
+Cure::Cure(Cure const & other): AMateria("cure")
 {
-	
+	return;
 }
 
 Cure::~Cure(void)
@@ -81,4 +81,152 @@ AMateria *Cure::clone(void) const
 void Cure::use(ICharacter &target)
 {
 	std::cout << "* heals " << target.getName() << " *" << std::endl;
+}
+
+
+
+
+Character::Character(std::string name) : _name(name)
+{
+	for (int i = 0; i < 4; i++)
+		this->_materia[i] = NULL;
+}
+
+Character::Character(Character const &toCopy) : _name(toCopy.getName())
+{
+	int i;
+	for (i = 0; i < 4; i++)
+	{
+		if (this->_materia[i])
+			delete (this->_materia[i]);
+	}
+	for (i = 0; i < 4; i++)
+	{
+		if (toCopy._materia[i])
+			this->_materia[i] = toCopy._materia[i]->clone();
+	}
+		
+}
+
+Character & Character::operator=(Character const &toAssign)
+{
+	this->_name = toAssign.getName();
+	for (int i = 0; i < 4; i++)
+	{
+		if (toAssign._materia[i])
+			this->_materia[i] = toAssign._materia[i];
+		else
+			this->_materia[i] = NULL;
+	}
+	return (*this);
+}
+
+Character::~Character(void)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		if (this->_materia[i])
+			delete (this->_materia[i]);
+	}
+		
+}
+
+std::string const &	Character::getName(void) const
+{
+	return (this->_name);
+}
+
+void Character::equip(AMateria* m)
+{
+	int i = 0;
+	while (i < 4)
+	{
+		if (this->_materia[i] == NULL)
+		{
+			this->_materia[i] = m;
+			break;
+		}
+		i++;
+	}
+}
+
+void Character::unequip(int idx)
+{
+	if (idx >= 0 && idx < 4)
+		this->_materia[idx] = NULL;
+}
+
+void Character::use(int idx, ICharacter& target)
+{
+	if (idx >= 0 && idx < 4)
+	{
+		if (this->_materia[idx])
+			this->_materia[idx]->use(target);
+		else
+			std::cout << "Index empty." << std::endl;
+	}
+	else
+		std::cout << "Index out of range." << std::endl;
+}
+
+
+
+MateriaSource::MateriaSource(void)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		this->_store[i] = NULL;
+	}
+}
+
+MateriaSource::MateriaSource(MateriaSource const &toCopy)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		if (toCopy._store[i])
+			this->_store[i] = toCopy._store[i];
+	}
+}
+
+MateriaSource &MateriaSource::operator=(MateriaSource const &toAssign)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		if (toAssign._store[i])
+			this->_store[i] = toAssign._store[i];
+	}
+	return (*this);
+}
+
+MateriaSource::~MateriaSource(void)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		if (this->_store[i])
+			delete (this->_store[i]);
+	}
+}
+
+void MateriaSource::learnMateria(AMateria *m)
+{
+	int i = 0;
+	while (i < 4)
+	{
+		if (this->_store[i] == NULL)
+		{
+			this->_store[i] = m;
+			break;
+		}
+		i++;
+	}
+}
+
+AMateria *MateriaSource::createMateria(std::string const &type)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		if (this->_store[i] && this->_store[i]->getType() == type)
+			return (this->_store[i]->clone());
+	}
+	return (0);
 }
--- CPP4/ex03/AMateria.hpp
+++ CPP4/ex03/AMateria.hpp
@@ -3,13 +3,21 @@
 
 #include <iostream>
 
+class AMateria;
+class Ice;
+class Cure;
+class ICharacter;
+class Character;
+class IMateriaSource;
+class MateriaSource;
+
 class AMateria
 {
     protected:
         std::string const _type;
     public:
         AMateria(std::string const & type);
-        ~AMateria(void);
+        virtual ~AMateria(void);
         std::string const & getType() const; //Returns the materia type
         virtual AMateria* clone() const = 0;
         virtual void use(ICharacter& target);
@@ -49,12 +57,43 @@ class ICharacter
         virtual void use(int idx, ICharacter& target) = 0;
 };
 
+class Character : public ICharacter
+{
+    private:
+        std::string _name;
+        AMateria *_materia[4];
+    public:
+        Character(std::string name);
+        Character (Character const &toCopy);
+        virtual ~Character();
+        Character &operator=(Character const &toAssign);
+
+        std::string const & getName() const;
+        void equip(AMateria* m);
+        void unequip(int idx);
+        void use(int idx, ICharacter& target);
+};
+
 class IMateriaSource
 {
     public:
         virtual ~IMateriaSource() {}
         virtual void learnMateria(AMateria*) = 0;
         virtual AMateria* createMateria(std::string const & type) = 0;
+};
+
+class MateriaSource : public IMateriaSource
+{
+    private:
+        AMateria *_store[4];
+    public:
+        MateriaSource(void);
+        MateriaSource(MateriaSource const &toCopy);
+        MateriaSource &operator=(MateriaSource const &toAssign);
+        virtual ~MateriaSource();
+
+        void learnMateria(AMateria *m);
+        AMateria *createMateria(std::string const & type);
 };
 
 #endif
